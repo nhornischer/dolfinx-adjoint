b76e01a0b8d8a96430c78632dc5e0d288bef993c
Niklas Hornischer <st161340@stud.uni-stuttgart.de>
2023-07-11 11:11:34 +0000

    add Poisson demo, stokes demo in development

---
+++ demo_Poisson.py
@@ -0,0 +1,158 @@
+"""
+In this demo we consider the parametric Poisson's equation with homogeneous Dirichlet boundary conditions:
+    - ν ∆u = f              in Ω
+         u = 0              on ∂Ω
+where Ω = [0,1]² is the unit square, ν = 1 is the diffusion coefficient and f is a parameter.
+We define the objective function (or quantity of interest)
+    J(u) = ½∫_Ω ||u - g|| dx
+where g is a known function.
+
+The goal of this demo is to compute the gradient of J(u) with respect to f using the adjoint method.
+
+We first start with the forward approach to solving the parametric Poisson's equation.
+The weak formulation of the problem reads: find u ∈ V such that
+    a(u, v) = L(v) ∀ v ∈ V
+where
+    a(u, v) = ∫_Ω ν ∇u · ∇v dx
+    L(v)    = ∫_Ω f v dx.
+
+We solve this problem with a residual equation
+    F(u) = a(u, v) - L(v) != 0
+"""
+
+import numpy as np
+import scipy.sparse as sps
+
+from mpi4py import MPI
+from dolfinx import mesh, fem, io, nls
+
+from petsc4py.PETSc import ScalarType
+
+import ufl
+
+# Define mesh and finite element space
+domain = mesh.create_unit_square(MPI.COMM_WORLD, 64, 64, mesh.CellType.triangle)
+V = fem.FunctionSpace(domain, ("CG", 1))
+W = fem.FunctionSpace(domain, ("DG", 0))
+
+# Define the boundary and the boundary conditions
+domain.topology.create_connectivity(domain.topology.dim -1, domain.topology.dim)
+boundary_facets = mesh.exterior_facet_indices(domain.topology)
+
+uD = fem.Function(V)
+uD.interpolate(lambda x: 0.0 * x[0])
+
+boundary_dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1, boundary_facets)
+bc = fem.dirichletbc(uD, boundary_dofs)
+
+# Define the basis functions and parameters
+uh = fem.Function(V, name="uₕ")
+v = ufl.TestFunction(V)
+f = fem.Function(W, name="f")
+nu = fem.Constant(domain, ScalarType(1.0))
+f.interpolate(lambda x: x[0] + x[1])
+
+# Define the variational form and the residual equation
+a = nu * ufl.inner(ufl.grad(uh), ufl.grad(v)) * ufl.dx
+L = f * v * ufl.dx
+
+F = a - L
+
+# Define the problem solver
+problem = fem.petsc.NonlinearProblem(F, uh, bcs=[bc])
+solver = nls.petsc.NewtonSolver(MPI.COMM_WORLD, problem)
+
+# Solve the problem
+solver.solve(uh)
+
+# Define profile g
+g = fem.Function(W)
+g.interpolate(lambda x: 1 / (2 * np.pi**2) * np.sin(np.pi * x[0]) * np.sin(np.pi * x[1]))
+
+# Define the objective function
+J = 0.5 * ufl.inner(uh - g, uh - g) * ufl.dx
+print("J(u) = ", fem.assemble_scalar(fem.form(J)))
+
+"""
+We now turn to the adjoint approach to calculating the gradient of J(u) with respect to f.
+Therefore, we first define a new reduced functional R(f) = J(u(f),f) and take the derivative with respect to f:
+    dJ/df = dR/df = ∂J/∂u * du/df + ∂J/∂f                       (1)
+
+The first term ∂J/∂u and the last term ∂J/∂f are easy to compute.
+The second term du/df will be handled using the adjoint problem.
+
+By taking the derivative of F(u) = 0 with respect to f, we obtain a representation of du/df:
+    dF/df = ∂F/∂u * du/df + ∂F/∂f = 0
+    => du/df = -(∂F/∂u)^-1 * ∂F/∂f                              (2)
+
+Inserting (2) into (1) yields
+
+    dJ/df = - ∂J/∂u * (∂F/∂u)^-1 * ∂F/∂f + ∂J/∂f                (3)
+
+Our adjoint approach thus is to solve the adjoint problem
+    ∂Fᵀ/∂u * λ =  - ∂Jᵀ/∂u                                      (4)
+and then compute the gradient of J(u) with respect to f using (3).
+    dJ/df = λᵀ * ∂F/∂f + ∂J/∂f                                  (5)   
+
+In our case ∂J/∂f = 0, so we only need to compute λᵀ * ∂F/∂f.
+"""
+def gradient():
+    dFdu = fem.assemble_matrix(fem.form(ufl.derivative(F, uh)), bcs=[bc])
+    dFdu.finalize()
+    shape = (V.dofmap.index_map.size_global, V.dofmap.index_map.size_global)
+    dFduSparse = sps.csr_matrix((dFdu.data, dFdu.indices, dFdu.indptr), shape=shape).transpose()
+
+    dJdu = - fem.assemble_vector(fem.form(ufl.derivative(J, uh))).array.transpose()
+
+    adjoint_solution = sps.linalg.spsolve(dFduSparse, dJdu)
+
+    dFdf = fem.assemble_matrix(fem.form(ufl.derivative(F, f))).to_dense()
+
+    gradient = adjoint_solution.transpose() @ dFdf
+
+    dJ = fem.Function(W, name="dJdf")
+    dJ.vector.setArray(gradient)
+    return dJ
+
+dJ = gradient()
+
+with io.XDMFFile(domain.comm, "demo_Poisson.xdmf", "w") as xdmf:
+    xdmf.write_mesh(domain)
+    xdmf.write_function(uh, 0.0)
+    xdmf.write_function(dJ, 0.0)
+
+"""
+In order to verify the compute gradient we perform a convergence study.
+Using a Taylor expansion of R(f) around a perturbation δf, we obtain
+    || R(f + ϵδf) - R(f)|| → 0 with O(ϵ).
+However, with the gradient we obtain quadratic convergence
+    || R(f + ϵδf) - R(f) - ϵ dR/df ⋅ δf|| → 0 with O(ϵ²).
+"""
+
+# Define the perturbation
+delta = fem.Function(W, name="δf")
+delta.interpolate(lambda x: x[0] * x[1])
+J_value = fem.assemble_scalar(fem.form(J))
+
+values_linear = []
+values_quadratic = []
+for epsilon in np.linspace(1, 0, 10):
+    f.interpolate(lambda x: x[0] + x[1] + epsilon * (x[0] * x[1]))
+    solver.solve(uh)
+    J_eps = fem.assemble_scalar(fem.form(J))
+    dJ = gradient()
+
+    values_linear.append(np.abs(J_eps - J_value))
+    values_quadratic.append(np.abs(J_eps - J_value - epsilon * dJ.vector.array.transpose() @ delta.vector.array))
+
+print("Linear convergence: ", values_linear)
+print("Quadratic convergence: ", values_quadratic)
+
+import matplotlib.pyplot as plt
+plt.figure(tight_layout=True)
+plt.plot(values_linear, label="linear")
+plt.plot(values_quadratic, label="quadratic")
+plt.xticks([0, 9], [1, 0])
+plt.legend()
+plt.show()
+
---
+++ demo_Stokes.py
@@ -0,0 +1,194 @@
+"""
+In this demo we consider the Stokes equation
+    - ν ∆u + ∇p = 0 in Ω            
+          ∇ · u = 0 in Ω
+with Dirichlet boundary conditions
+            u = g on circle boundary
+            u = f on inlet boundary
+            u = 0 on wall boundaries
+            p = 0 on outlet boundary
+
+u : Ω → R² is the unknown velocity field
+p : Ω → R is the unknwon pressure field
+ν : Ω → R is the viscosity field
+f : Ω → R² is the inlet velocity boudary condition
+g : Ω → R² is the circle velocity boundary condition
+
+The domain Ω is defined as a rectangle with a circle cut out of it, similar to the DFG 2D-3 benchmark of Flow past a cylinder
+The inlet boundary is the left boundary, the outlet boundary is the right boundary,
+and the wall boundaries are the top and bottom boundaries.
+
+We define the objective function (or quantity of interest)
+    J(u) = ½∫_Ω ∇u · ∇u dx + ½α∫_∂Ω_Circle g² ds
+with the regularization parameter α = 10.
+
+The weak formulation of the problem reads, find (u, p) ∈ V × Q such that
+    a((u, p), (v, q)) = L((v, q)) ∀ (v, q) ∈ V × Q
+where
+    a((u, p), (v, q)) = ∫_Ω ν ∇u · ∇v dx - ∫_Ω ∇ · v p dx - ∫_Ω ∇ · u q dx
+              L(v, q) = 0
+
+We solve this problem with a residual equation
+    F((u, p)) = a((u, p), (v, q)) - L((v, q)) != 0
+"""
+
+import numpy as np
+import scipy.sparse as sps
+import gmsh
+
+from mpi4py import MPI
+from dolfinx import mesh, fem, io, nls
+import ufl
+
+# Mesh parameters
+gmsh.initialize()
+L = 30
+H = 10
+c_x = 10
+c_y = 5
+r = 2.5
+gdim = 2
+mesh_comm = MPI.COMM_WORLD
+model_rank = 0
+
+fluid_marker = 1
+inlet_marker, outlet_marker, wall_marker, obstacle_marker = 2, 3, 4, 5
+inflow, outflow, walls, obstacle = [], [], [], []
+
+# Mesh generation
+if mesh_comm.rank == model_rank:
+    rectangle = gmsh.model.occ.addRectangle(0,0,0, L, H, tag=1)
+    circle = gmsh.model.occ.addDisk(c_x, c_y, 0, r, r)
+    fluid = gmsh.model.occ.cut([(gdim, rectangle)], [(gdim, circle)])
+    gmsh.model.occ.synchronize()
+
+    volumes = gmsh.model.getEntities(dim=gdim)
+    assert(len(volumes) == 1)
+    gmsh.model.addPhysicalGroup(volumes[0][0], [volumes[0][1]], fluid_marker)
+    gmsh.model.setPhysicalName(volumes[0][0], fluid_marker, "Fluid")
+
+    boundaries = gmsh.model.getBoundary(volumes, oriented=False)
+    for boundary in boundaries:
+        center_of_mass = gmsh.model.occ.getCenterOfMass(boundary[0], boundary[1])
+        if np.allclose(center_of_mass, [0, H/2, 0]):
+            inflow.append(boundary[1])
+        elif np.allclose(center_of_mass, [L, H/2, 0]):
+            outflow.append(boundary[1])
+        elif np.allclose(center_of_mass, [L/2, H, 0]) or np.allclose(center_of_mass, [L/2, 0, 0]):
+            walls.append(boundary[1])
+        else:
+            obstacle.append(boundary[1])
+    gmsh.model.addPhysicalGroup(1, walls, wall_marker)
+    gmsh.model.setPhysicalName(1, wall_marker, "Walls")
+    gmsh.model.addPhysicalGroup(1, inflow, inlet_marker)
+    gmsh.model.setPhysicalName(1, inlet_marker, "Inlet")
+    gmsh.model.addPhysicalGroup(1, outflow, outlet_marker)
+    gmsh.model.setPhysicalName(1, outlet_marker, "Outlet")
+    gmsh.model.addPhysicalGroup(1, obstacle, obstacle_marker)
+    gmsh.model.setPhysicalName(1, obstacle_marker, "Obstacle")
+
+    gmsh.option.setNumber("Mesh.Algorithm", 8)
+    gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2)
+    gmsh.option.setNumber("Mesh.RecombineAll", 1)
+    gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
+    gmsh.option.setNumber("Mesh.CharacteristicLengthMin",0.5)
+    gmsh.option.setNumber("Mesh.CharacteristicLengthMax",1.0)
+    gmsh.model.mesh.generate(gdim)
+    gmsh.model.mesh.setOrder(2)
+    gmsh.model.mesh.optimize("Netgen")
+
+mesh, _, ft = io.gmshio.model_to_mesh(gmsh.model, mesh_comm, model_rank, gdim=gdim)
+ft.name = "Facet markers"
+
+# Parameters
+nu = 1.0
+alpha = 10.0
+
+# Function spaces as Mixed Element space
+u_elem = ufl.VectorElement("CG", mesh.ufl_cell(), 2)
+p_elem = ufl.FiniteElement("CG", mesh.ufl_cell(), 1)
+
+v_elem = ufl.MixedElement([u_elem, p_elem])
+V = fem.FunctionSpace(mesh, v_elem)
+V_u, _ = V.sub(0).collapse()
+V_p, _ = V.sub(1).collapse()
+
+up = fem.Function(V)
+(u, p) = ufl.split(up)
+u_, p_ = ufl.TrialFunctions(V)
+
+vq = ufl.TestFunction(V)
+(v, q) = ufl.split(vq)
+
+# Boundary conditions   
+f = fem.Function(V_u)             # Inflow Dirichlet boundary condition
+f.interpolate(lambda x: np.stack((x[1]*(10-x[1])/25, 0.0* x[0])))
+g = fem.Function(V_u)             # Circle Dirichlet boundary condition
+noslip = fem.Function(V_u)        # No-slip homogenous Dirichlet boundary condition at the walls for the velocity
+outflow = fem.Function(V_p)       # Outflow homogeneous Dirichlet boundary condition for the pressure
+
+dofs_walls = fem.locate_dofs_topological((V.sub(0), V_u), 1, ft.indices[ft.values == wall_marker])
+dofs_inflow = fem.locate_dofs_topological((V.sub(0), V_u), 1, ft.indices[ft.values == inlet_marker])
+dofs_outflow = fem.locate_dofs_topological((V.sub(1), V_p), 1, ft.indices[ft.values == outlet_marker])
+dofs_obstacle = fem.locate_dofs_topological((V.sub(0), V_u), 1, ft.indices[ft.values == obstacle_marker])
+
+bcs = [fem.dirichletbc(f, dofs_inflow, V.sub(0)), fem.dirichletbc(g, dofs_obstacle, V.sub(0)), fem.dirichletbc(noslip, dofs_walls, V.sub(0)), fem.dirichletbc(outflow, dofs_outflow, V.sub(1))]
+
+# Variational formulation
+F = nu * ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx\
+    - ufl.div(v) * p * ufl.dx\
+    - q * ufl.div(u) * ufl.dx
+
+# Define the problem solver
+problem = fem.petsc.NonlinearProblem(F, up, bcs=bcs)
+solver = nls.petsc.NewtonSolver(MPI.COMM_WORLD, problem)
+
+solver.solve(up)
+
+# Define the objective function
+dObs = ufl.Measure("ds", domain=mesh, subdomain_data=ft, subdomain_id=obstacle_marker)
+J  = 0.5 * ufl.inner(ufl.grad(u), ufl.grad(u)) * ufl.dx + alpha / 2 * ufl.inner(g, g) * dObs
+
+print("J(u, p) = ", fem.assemble_scalar(fem.form(J)))
+
+""" Calculate the gradient with an adjoint method
+        dJdg = ∂J/∂u * du/dg + ∂J/∂g 
+    where we have no terms of the pressure.
+
+    This results in
+        dJ/dg = - ∂J/∂u * (∂F/∂u)^-1 * ∂F/∂g + ∂J/∂g   
+
+    We thus solve for the adjoint variable λ:
+        ∂Fᵀ/∂u * λ =  - ∂Jᵀ/∂u 
+    and then calculate the gradient as
+        dJ/dg = λᵀ * ∂F/∂g + ∂J/∂g 
+"""
+
+dFdu = fem.assemble_matrix(fem.form(ufl.derivative(F, u, u_)), bcs = bcs)
+dFdu.finalize()
+shape = (V.dofmap.index_map.size_global, V.dofmap.index_map.size_global)
+dFduSparse = sps.csr_matrix((dFdu.data, dFdu.indices, dFdu.indptr), shape=shape).transpose()
+
+dJdu = - fem.assemble_vector(fem.form(ufl.derivative(J, u, u_))).array.transpose()
+
+adjoint_solution = sps.linalg.spsolve(dFduSparse, dJdu)
+
+dJdg = fem.assemble_vector(fem.form(ufl.derivative(J, g)))
+
+dFdg = fem.assemble_matrix(fem.form(ufl.derivative(F, g)))
+
+
+
+gradient = adjoint_solution.transpose() @ dFdg + dJdg
+
+dJ = fem.Function(V_u, name="dJdg")
+dJ.vector.setArray(gradient)
+
+with io.XDMFFile(mesh.comm, "demo_Stokes.xdmf", "w") as xdmf:
+    u.name = "Velocity"
+    p.name = "Pressure"
+    dJ.name = "Gradient"
+    xdmf.write_mesh(mesh)
+    xdmf.write_function(u, 0.0)
+    xdmf.write_function(p, 0.0)
+    xdmf.write_function(dJ, 0.0)
